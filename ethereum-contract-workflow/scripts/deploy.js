6f5e5495a40dce38e5c30f508b7a7b52980ccde1
liushuiyuan <13783534092@sina.cn>
2023-05-07 23:35:15 +0800

    update

--- ethereum-contract-workflow/scripts/deploy.js
+++ ethereum-contract-workflow/scripts/deploy.js
@@ -1,6 +1,7 @@
 const path = require('path')
-const Web3 = require('web3')
-const HDWalletProvider = require('truffle-hdwallet-provider')
+const { Web3 } = require('web3');
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 //1. 拿到 bytecode
 const contractPath = path.resolve(__dirname, '../compiled/Car.sol.json')
@@ -8,10 +9,12 @@ const { abi, evm } = require(contractPath).Car
 const bytecode = evm.bytecode.object
 
 //2. 配置 provider
-const provider = new HDWalletProvider(
-  'chief catalog bleak rigid nest town awkward major barrel stairs lobster police',
-  'https://goerli.infura.io/v3/265996e92f8140da95f6df7055737dfe'
-)
+const provider = new HDWalletProvider({
+  mnemonic: {
+    phrase: 'chief catalog bleak rigid nest town awkward major barrel stairs lobster police'
+  },
+  providerOrUrl: 'https://eth-goerli.g.alchemy.com/v2/Kmr39z1LwpqDkgqPlOIYcmZ9n2iqonvJ'
+})
 
 //3. 初始化 web3 实例
 const web3 = new Web3(provider)
@@ -22,22 +25,22 @@ const deploy = async () => {
   console.log('部署合约账户：', accounts[0])
   // 5. 创建合约实例并且部署
   console.time('contract-deploy');
-  const result = await new web3.eth.Contract(abi)
+  let result = null
+  try {
+    result = await new web3.eth.Contract(abi)
     .deploy({
       data: bytecode,
       arguments: ['BWM']
     })
     .send({
       from: accounts[0],
-      gas: 500000,
-      gasPrice: "100000",
+      gas: 6680,
+      // gasPrice: "1000000000",
     })
-    .on('receipt', function(receipt){
-      console.log('receipt', receipt);
-    })
-    .on('error', function(error){
-      console.log(error);
-    })
+  } catch (error) {
+    console.log('error', error)
+  }
+
   console.timeEnd('contract-deploy');
 
   console.log('合约部署成功', result?.options?.address)
